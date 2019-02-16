a889b36849be01b1debf4822f4ea0a43170e6576
Charles Xie <charxie@gmail.com>
2019-02-16 14:16:06 -0500

    Java controls GPIO

--- src/main/java/org/concord/iot/Hello.java
+++ src/main/java/org/concord/iot/Hello.java
@@ -8,16 +8,43 @@ import com.google.firebase.database.*;
 import java.io.FileInputStream;
 import java.io.IOException;
 
-import com.pi4j.io.gpio.GpioController;
-import com.pi4j.io.gpio.GpioFactory;
+import com.pi4j.io.gpio.*;
+import com.pi4j.io.gpio.event.GpioPinDigitalStateChangeEvent;
+import com.pi4j.io.gpio.event.GpioPinListenerDigital;
 
 public class Hello {
 
     public static void main(final String[] args) {
 
-        System.out.println("Hello");
-
         final GpioController gpio = GpioFactory.getInstance();
+        GpioPinDigitalInput buttonA = gpio.provisionDigitalInputPin(RaspiPin.GPIO_29, "Button A", PinPullResistance.PULL_DOWN);
+        GpioPinDigitalInput buttonB = gpio.provisionDigitalInputPin(RaspiPin.GPIO_28, "Button B", PinPullResistance.PULL_DOWN);
+        GpioPinDigitalInput buttonC = gpio.provisionDigitalInputPin(RaspiPin.GPIO_27, "Button C", PinPullResistance.PULL_DOWN);
+        GpioPinDigitalOutput redLed = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_22, "Red LED", PinState.HIGH);
+        GpioPinDigitalOutput greenLed = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_24, "Green LED", PinState.HIGH);
+        GpioPinDigitalOutput blueLed = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_25, "Blue LED", PinState.HIGH);
+
+        System.out.println("Hello " + redLed.isHigh() + ", " + greenLed.isHigh() + ", " + blueLed.isHigh() + ", " + buttonA.isHigh() + ", " + buttonB.isHigh() + ", " + buttonC.isHigh());
+        //redLed.blink(1000);
+        //greenLed.blink(1000);
+        //blueLed.blink(1000);
+
+        GpioPinListenerDigital listener = new GpioPinListenerDigital() {
+            @Override
+            public void handleGpioPinDigitalStateChangeEvent(GpioPinDigitalStateChangeEvent event) {
+                GpioPin pin = event.getPin();
+                PinState pinState = event.getState();
+                System.out.println(" --> GPIO PIN STATE CHANGE: " + pin + " = " + pinState);
+                if (pinState.isLow()) {
+                    redLed.low();
+                } else {
+                    redLed.high();
+                }
+            }
+        };
+        buttonA.addListener(listener);
+        buttonB.addListener(listener);
+        buttonC.addListener(listener);
 
         try {
             FileInputStream serviceAccount = new FileInputStream("raspberry-pi-java-firebase-adminsdk-eeeo1-f7e5dc2054.json");
