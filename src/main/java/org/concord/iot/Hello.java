464480ea1f4eb30e255f5da23d31c3cc7683a56e
Charles Xie <charxie@gmail.com>
2019-02-15 21:53:36 -0500

    Initial commit

---
+++ src/main/java/org/concord/iot/Hello.java
@@ -0,0 +1,51 @@
+package org.concord.iot;
+
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import com.google.firebase.database.*;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+
+public class Hello {
+
+    public static void main(final String[] args) {
+
+        System.out.println("Hello");
+
+        try {
+            FileInputStream serviceAccount = new FileInputStream("raspberry-pi-java-firebase-adminsdk-eeeo1-f7e5dc2054.json");
+            FirebaseOptions options = new FirebaseOptions.Builder()
+                    .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+                    .setDatabaseUrl("https://raspberry-pi-java.firebaseio.com")
+                    .build();
+            FirebaseApp.initializeApp(options);
+            final FirebaseDatabase database = FirebaseDatabase.getInstance();
+            DatabaseReference ref = database.getReference("Name");
+            ref.addValueEventListener(new ValueEventListener() {
+                @Override
+                public void onDataChange(DataSnapshot dataSnapshot) { // This method is called once with the initial value and again whenever data at this location is updated.
+                    System.out.println(dataSnapshot.getValue());
+                }
+
+                @Override
+                public void onCancelled(DatabaseError error) {
+                    System.out.println("Failed to read value: " + error.toException());
+                }
+            });
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        while(true){
+            try {
+                Thread.currentThread().sleep(1000);
+            } catch (InterruptedException e){
+                e.printStackTrace();
+            }
+        }
+
+    }
+
+}
